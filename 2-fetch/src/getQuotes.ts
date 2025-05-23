b35d57f0aa97f440e32405f20fccb593ec57e828
sarrabah <sarrabenarbia1996@gmail.com>
2025-05-23 14:05:51 +0200

    getQuotes function

--- 2-fetch/src/getQuotes.ts
+++ 2-fetch/src/getQuotes.ts
@@ -1,4 +1,19 @@
-export const getQuotes = async () => {
-  const res = await fetch("https://dummyjson.com/quotes");
-  return res.json();
+interface Quote {
+  id: number;
+  quote: string;
+  author: string
+}
+interface QuotesDetails{
+  quotes: Quote[];
+  total: number;
+  skip: number;
+  limit: number
+}
+
+export const getQuotes  = async (): Promise<QuotesDetails> => {
+  const res = await fetch("https://dummyjson.com/quotes"); 
+  const data: QuotesDetails = await res.json()
+  return data;
 };
+
+
